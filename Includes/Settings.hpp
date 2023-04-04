137d27905cce0146888cfbe9a6552358faba1822
Hamza Chakoubi <hamza.chakoubi28@gmail.com>
2023-04-04 16:04:18 +0000

    not initializing request method bug fixed

--- Includes/Settings.hpp
+++ Includes/Settings.hpp
@@ -6,7 +6,7 @@
 /*   By: hchakoub <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 15:34:07 by hchakoub          #+#    #+#             */
-/*   Updated: 2023/03/29 17:42:00 by hchakoub         ###   ########.fr       */
+/*   Updated: 2023/04/04 15:56:27 by hchakoub         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,12 +29,12 @@ class Settings {
     Settings();
     ~Settings();
     static Settings *object_;
-    std::map<std::string, int> request_methods_;
+    std::map<std::string, Request_Method_e> request_methods_;
     void setRequestMethods_();
 
   public:
     static Settings* boot();
     static Settings* get();
-    int indexOfRequestMethod(const std::string& method);
+    Request_Method_e indexOfRequestMethod(const std::string& method);
 };
 #endif
