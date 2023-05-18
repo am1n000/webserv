21a86c26f6236ccc53db60e7ae71b0343de544e6
webser <el.rhair.amine@gmail.com>
2023-05-18 04:14:08 -0400

    testing to push from vm

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include "Includes/App.hpp"
 #include "Includes/Config.hpp"
-#include "Includes/multiplexers/Kqueue.hpp"
+//#include "Includes/multiplexers/Kqueue.hpp"
 #include "Includes/multiplexers/Select.hpp"
 #include "Includes/multiplexers/Poll.hpp"
 
@@ -8,8 +8,8 @@ int main ()
 {
 	app();
   	std::vector<Server *> servers = Config::get()->getServers();
-	Kqueue *K = Kqueue::getInstance();
-	K->serve();
+//	Kqueue *K = Kqueue::getInstance();
+//	K->serve();
 	Select *S = Select::getInstance();
 	S->serve();
 	Poll *P = Poll::getInstance();
