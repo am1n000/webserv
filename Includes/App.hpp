a1a8d6254f994d89d5a64137690e52d7fb89b8af
Amine El rhair <ael-rhai@e2r7p1.1337.ma>
2023-05-24 06:35:56 +0100

    modified main to take two arguments

--- Includes/App.hpp
+++ Includes/App.hpp
@@ -1,6 +1,13 @@
-
-
 #ifndef APP_HPP
 #define APP_HPP
-void app();
+
+
+#include "Config.hpp"
+#include "Settings.hpp"
+#include "multiplexers/Kqueue.hpp"
+#include "multiplexers/Epoll.hpp"
+#include "multiplexers/Select.hpp"
+#include "multiplexers/Poll.hpp"
+
+void app(char *config_file, int flag);
 #endif // !APP_hpp
